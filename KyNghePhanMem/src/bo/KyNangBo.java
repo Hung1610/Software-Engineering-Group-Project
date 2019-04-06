1fe220ee91835335b02e62291b1d394bb3a62592
Heggman <phuochung008@gmail.com>
2019-04-06 21:28:29 +0700

    Fix.

--- KyNghePhanMem/src/bo/KyNangBo.java
+++ KyNghePhanMem/src/bo/KyNangBo.java
@@ -9,6 +9,7 @@ public class KyNangBo {
 	ThongTinKyNangDao kynangdao= new ThongTinKyNangDao();
 	public ArrayList<KyNangBean> knlist;
 	
+	// Thêm kỹ năng vào SQL
 	public int Them(String MaKyNang, String TenKyNang, String ChiTiet) throws Exception{
 		
 		for(KyNangBean thongtin : knlist) {
@@ -16,20 +17,24 @@ public class KyNangBo {
 				return 0;
 		}
 		kynangdao.Them(MaKyNang, TenKyNang, ChiTiet);
+		knlist = getKyNang();
 		return 1;
 	}
 	
+	// Lấy toàn bộ kỹ năng có sẵn trong SQL
 	public ArrayList<KyNangBean> getKyNang() throws Exception{
 		knlist=kynangdao.getKyNang();
 		return knlist;
 	}
 	
+	// Xóa kỹ năng theo mã đưa vào
 	public int Xoa(String ma) throws Exception{
 		int kq = kynangdao.Xoa(ma);
 		knlist = getKyNang();
 		return kq;
 	}
 	
+	// Cập nhật kỹ năng theo mã đưa vào
 	public int CapNhat(String MaKyNang, String TenKyNang, String ChiTiet) throws Exception{
 		int kq = kynangdao.CapNhat(MaKyNang,TenKyNang,ChiTiet);
 		knlist = getKyNang();
--- KyNghePhanMem/src/bo/NhanVienBo.java
+++ KyNghePhanMem/src/bo/NhanVienBo.java
@@ -10,48 +10,34 @@ public class NhanVienBo {
 	NhanVienDao nhanviendao= new NhanVienDao();
 	public ArrayList<NhanVien> nv;
 	
+	// Thêm nhân viên vào SQL
 	public int Them(String MaNhanVien, String TenNhanVien,Date NgaySinh, String DiaChi, String Sdt, String Email) throws Exception{
-		
 		for(NhanVien thongtin : nv) {
 			if(thongtin.getMaNhanVien().equals(MaNhanVien)) 
 				return 0;
-			else {
-				nhanviendao.Them(MaNhanVien, TenNhanVien, NgaySinh, DiaChi, Sdt, Email);
-				return 1;
-				
-			}
 		}
+		nhanviendao.Them(MaNhanVien, TenNhanVien, NgaySinh, DiaChi, Sdt, Email);
+		nv = getNhanVien();
 		return 1;
 	}
 	
+	// Lấy toàn bộ nhân viên từ SQL
 	public ArrayList<NhanVien> getNhanVien() throws Exception{
-		nv=nhanviendao.getNhanVien();
+		nv= nhanviendao.getNhanVien();
 		return nv;
 	}
 	
+	// Xóa nhân viên theo mã đưa vào
 	public int Xoa(String ma) throws Exception{
 		int kq = nhanviendao.Xoa(ma);
-		for(NhanVien thongtin : nv) {
-			if (thongtin.getMaNhanVien().equals(ma)) {
-				nv.remove(thongtin);
-				break;
-			}
-		}
+		nv= getNhanVien();
 		return kq;
 	}
 	
+	// Cập nhật nhân viên theo mã đưa vào
 	public int CapNhat(String MaNhanVien, String TenNhanVien, Date NgaySinh, String DiaChi, String Sdt, String Email) throws Exception{
 		int kq = nhanviendao.CapNhat(MaNhanVien, TenNhanVien, NgaySinh, DiaChi, Sdt, Email);
-		for(NhanVien thongtin : nv) {
-			if(thongtin.getMaNhanVien().equals(MaNhanVien)) {
-				thongtin.setTenNhanVien(TenNhanVien);
-				thongtin.setNgaySinh(NgaySinh);
-				thongtin.setDiaChi(DiaChi);
-				thongtin.setSdt(Sdt);
-				thongtin.setEmail(Email);
-			}
-			break;
-		}
+		nv= getNhanVien();
 		return kq;
 	}
 	
