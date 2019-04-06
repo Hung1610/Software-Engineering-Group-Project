2f986ad81215cc563a073fb9657d8f6ebff58e02
Phamvantuan <vantuankrn197@gmail.com>
2019-04-06 10:02:29 +0700

    Ky nang

--- KyNghePhanMem/src/dao/KyNangNhanVienDao.java
+++ KyNghePhanMem/src/dao/KyNangNhanVienDao.java
@@ -1,5 +1,22 @@
 package dao;
 
-public class KyNangNhanVienDao {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
+import bean.KyNangNhanVienBean;
+
+public class KyNangNhanVienDao {
+	public ArrayList<KyNangNhanVienBean> listSkill = new ArrayList<KyNangNhanVienBean>();
+	
+	public ArrayList<KyNangNhanVienBean> getListSkill() throws Exception
+	{
+		String sql = "select * from KyNangNhanVien";
+		PreparedStatement cmd = DungChung.cn.prepareStatement(sql);
+		ResultSet rs = cmd.executeQuery();
+		while(rs.next())
+		{
+			string MaNV = 
+		}
+	}
 }
